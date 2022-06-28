ecbf4ac8d16f02ecf7b3037552e145090c83be23
Sergey Dolgunov <dolgunov.s@gmail.com>
2022-06-28 21:21:27 +0300

    Find max from 3 numbers

---
+++ workshop1/task#4/Program.cs
@@ -0,0 +1,35 @@
+﻿/* **Задача 4:** Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+
+44 5 78 -> 78
+
+22 3 9 -> 22 */
+
+Console.WriteLine("Введите первое целое число ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число ");
+int third_number = Convert.ToInt32(Console.ReadLine());
+int max;
+
+Console.WriteLine();
+if (first_number > second_number) 
+{
+    max = first_number;
+}  
+else 
+{
+    max = second_number;
+}
+if (third_number > max)
+{
+    Console.Write("Ответ: ");
+    Console.WriteLine($"max из трех чисел = {third_number}");
+}
+else
+{
+    Console.Write("Ответ: ");
+    Console.WriteLine($"max из трех чисел = {max}");
+}
