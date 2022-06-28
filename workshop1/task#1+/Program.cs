cb03b41cc71db4694a618864f61bd277e3c0a905
Sergey Dolgunov <dolgunov.s@gmail.com>
2022-06-28 21:05:07 +0300

    Function: find max and min from 2 numbers

---
+++ workshop1/task#1+/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// РЕШЕНИЕ С ФУНКЦИЕЙ:
+
+int MaxNumber(int number1, int number2)
+{
+    int max;
+    if (number1 > number2)
+    {
+        max = number1;
+    }
+    else
+    {
+        max = number2;
+    }
+    return max; 
+    
+}
+
+int MinNumber(int number1, int number2)
+{
+    int min;
+    if (number1 > number2)
+    {
+        min = number2;
+    }
+    else
+    {
+        min = number1;
+    }
+    return min; 
+    
+}
+
+
+Console.WriteLine("Введите первое целое число ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($" max из двух чисел = {MaxNumber(first_number, second_number)}");
+Console.WriteLine($" min из двух чисел = {MinNumber(first_number, second_number)}");
