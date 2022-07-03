d4ba833431167378e6deab8f87532c9abde132f8
Sergey Dolgunov <dolgunov.s@gmail.com>
2022-07-03 23:07:02 +0300

    add task#23: принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.

---
+++ workshop3/task#23/Program.cs
@@ -0,0 +1,21 @@
+﻿/* **Задача 23** 
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+*/
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Zadacha22(number);
+
+void Zadacha22(int arg)
+{
+    int count = 1;
+    Console.WriteLine();
+    Console.WriteLine($"таблица кубов чисел от 1 до {arg}:");
+    while (count <= arg)
+    {
+        Console.WriteLine(count * count * count);
+        count++;
+    }
+    Console.WriteLine();
+}
