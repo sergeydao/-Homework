d29ea339ec45f5e150a3d5ec414e4f03d910099e
Sergey Dolgunov <dolgunov.s@gmail.com>
2022-07-03 22:24:38 +0300

    add task#13: выводит третью цифру заданного числа или сообщает, что третьей цифры нет

---
+++ workshop2/task#13/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("Введите положительное целое число");
+string number = Console.ReadLine();
+Zadacha13(number);
+
+
+void Zadacha13(string arg)
+{
+    if (arg.Length < 3)
+    {
+        Console.WriteLine("Число состоит из двух цифр");
+    }
+    else
+    {
+        Console.WriteLine($"Третья цифра числа {arg} = {arg[2]}");
+    }
+}
